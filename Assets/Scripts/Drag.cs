22436462e00b27ecb5b97488096ec32bcab3cacb
PluseggWill <54594803+PluseggWill@users.noreply.github.com>
2020-01-26 23:00:38 -0500

    Upload

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDragHandler
+{
+    public Transform parentCanvas = null;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("onBeginDrag");
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        parentCanvas = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("onDrag");
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("onEndDrag");
+        this.transform.SetParent(parentCanvas);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drop : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Drag d = eventData.pointerDrag.GetComponent<Drag>();
+        if (d != null)
+        {
+            d.parentCanvas = this.transform;
+        }
+    }
+}
